3f2713c91d08a74a69d96450817c24871f049bff
Harvey Maunders <146084760+harveymmaunders@users.noreply.github.com>
2025-03-04 15:54:12 +0000

    Update build.gradle.kts

--- client-samples/java/websockets/build.gradle.kts
+++ client-samples/java/websockets/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     testImplementation(group="io.fabric8", name="mockwebserver", version="7.1.0")
     testImplementation(platform("org.junit:junit-bom:5.12.0"))
     testImplementation(group="org.junit.jupiter", name="junit-jupiter")
-    testRuntimeOnly(group="org.junit.platform", name="junit-platform-launcher
+    testRuntimeOnly(group="org.junit.platform", name="junit-platform-launcher")
     testImplementation(group="org.mockito", name="mockito-core", version="5.15.2")
 }
 
